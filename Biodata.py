330d6b917cfa0d89270c3886dfb554a5bcc8e8fe
ChoNg-YuN? <107825620+lluhfii@users.noreply.github.com>
2022-07-14 11:16:26 +0700

    Add files via upload

---
+++ Biodata.py
@@ -0,0 +1,10 @@
+nama_lengkap = input("masukkkan nama lengkap : ")
+asal_daerah = input("masukkan asal daerah : ")
+
+biodata = [ nama_lengkap, asal_daerah ]
+
+for isi_biodata in biodata :
+    print(isi_biodata)
+    
+# print(biodata[0])
+# print(biodata[1])
